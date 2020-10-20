713c3f25daf5436a0a0adbb93f7b7455c97781ed
Frieda Abdo <friedabdo@gmail.com>
2020-10-20 16:38:31 -0400

    server

---
+++ server.js
@@ -0,0 +1,37 @@
+//environmental vars
+require('dotenv').config()
+const {PORT = 3500, NODE_ENV = "development"}= process.env
+
+//CORS
+const cors = require("cors");
+const corsOptions = require("./configs/cors.js");
+
+//EXPRESS
+const express = require('express')
+const app = express()
+
+//Morgan
+const logger = require('morgan')
+app.use(logger('dev'));
+
+//other middleware
+NODE_ENV === "production" ? app.use(cors(corsOptions)) : app.use(cors());
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json({
+        status:200,
+        msg: 'you have hit the default route...nothing to see here'
+    })
+})
+
+const listsController = require('./controllers/lists.js')
+app.use('/allLists', listsController)
+
+const categoriesController = require('./controllers/categories.js')
+app.use('/categories',categoriesController)
+
+app.listen(PORT, () => {
+    console.log(`listening in on port: ${PORT}`)
+})
